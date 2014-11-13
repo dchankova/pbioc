719a9e37637bbb3369cc4618d8dab9cdf2c2b53d
nyxz <master033@gmail.com>
2014-11-13 08:07:47 +0200

    Fixed menu button slide.

--- public/js/pbioc.js
+++ public/js/pbioc.js
@@ -1,4 +1,8 @@
 $(function() {
     var s = skrollr.init({forceHeight : false});
     skrollr.menu.init(s, {});
+
+    $('#menu-toggle').click(function () {
+        $('body').toggleClass('menuActive');
+    });
 });
