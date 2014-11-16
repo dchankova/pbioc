a0343a3d47b6a9e71782d86821cdb980fe9391da
nyxz <master033@gmail.com>
2014-11-16 22:03:38 +0200

    Changed sections layouts

--- public/js/pbioc.js
+++ public/js/pbioc.js
@@ -1,6 +1,10 @@
 $(function() {
+	var scrollDuration = function(currentTop, targetTop) {
+		return 1000;
+	}
+
     var s = skrollr.init({forceHeight : false});
-    skrollr.menu.init(s, {});
+    skrollr.menu.init(s, {duration: scrollDuration});
 
     $('#menu-toggle').click(function () {
         $('body').toggleClass('menuActive');
